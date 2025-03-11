1a90757aefc3fa873f5eeb60ad1df5e3c8ea630f
Franqsinatra <frankiestalker@gmail.com>
2025-03-11 00:15:52 +0000

    init app commit

---
+++ MacVitals/MacVitalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacVitalsApp.swift
+//  MacVitals
+//
+//  Created by Frankie stalker on 11/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct MacVitalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MacVitalsTests/MacVitalsTests.swift
@@ -0,0 +1,17 @@
+//
+//  MacVitalsTests.swift
+//  MacVitalsTests
+//
+//  Created by Frankie stalker on 11/03/2025.
+//
+
+import Testing
+@testable import MacVitals
+
+struct MacVitalsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
