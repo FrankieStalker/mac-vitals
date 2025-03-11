e93cd75f12028ca3dd455fdb1dcce5010bbf1b09
Franqsinatra <frankiestalker@gmail.com>
2025-03-11 00:20:40 +0000

    Added basic widget

---
+++ MCWidget/MCWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  MCWidgetBundle.swift
+//  MCWidget
+//
+//  Created by Frankie stalker on 11/03/2025.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct MCWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        MCWidget()
+    }
+}
